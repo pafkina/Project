d1abaeed0d49710e221d80c5c1c9e5e265867cbf
pafkina <likalika0701@gmail.com>
2019-05-12 12:05:00 +0300

    check in

---
+++ laba4/Controllers/NewsFormController.cs
@@ -0,0 +1,42 @@
+﻿using laba4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace laba4.Controllers
+{
+    public class NewsFormController : Controller
+    {
+        // GET: NewsForm
+        [HttpGet]
+        public ActionResult News(int id = 0)
+        {
+            return View(new NewsModel());
+        }
+
+        [HttpPost]
+        public ActionResult News(NewsModel model)
+        {
+            using (DBModel db = new DBModel())
+            {
+                db.NewsModel.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }   
+            
+    
+        }
+
+        public ActionResult GetData()
+        {
+            using (DBModel db = new DBModel())
+            {
+                List<NewsModel> newsList = db.NewsModel.ToList<NewsModel>();
+                return Json(new { data = newsList }, JsonRequestBehavior.AllowGet); 
+            }
+        }
+        
+    }
+}
---
+++ laba4/Models/NewsModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;        
+using System.Linq;
+using System.Web;
+
+namespace laba4.Models
+{
+    public partial class NewsModel 
+    {
+        public int NewsID { get; set; }
+
+        [Required(ErrorMessage = "This field is Required")]
+        public string Locality { get; set; }
+
+        [Required(ErrorMessage = "This field is Required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "This field is Required")]
+        public string Description { get; set; }
+    }
+}
